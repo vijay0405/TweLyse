5cf24305294ea02dc0f523f9e57ccf79f123b0aa
vijay0405 <uvijaysimha@gmail.com>
2020-05-06 22:05:48 +0530

    added google nlp api for fetching score with private key

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TweLyse
 Twitter sentiment analysis tool.
+score of the sentiment ranges between -1.0 (negative) and 1.0 (positive) and corresponds to the overall emotional leaning of the text
--- index.js
+++ index.js
@@ -1,23 +1,56 @@
 const Twitter = require('twitter-lite');
 const keys = require('./keys.js');
+const language = require('@google-cloud/language');
+const languageClient = new language.LanguageServiceClient();
 
 const user = new Twitter({
     consumer_key: keys.APIkey,
     consumer_secret: keys.APIsecretkey,
 });
 
-(async function() {
+async function getSentimentScore(text) {
+    const document = {
+        content: text,
+        type: 'PLAIN_TEXT',
+    };
+
+    const [result] = await languageClient.analyzeSentiment({ document: document });
+    const sentiment = result.documentSentiment;
+    console.log(sentiment);
+    return sentiment.score;
+}
+
+
+async function checkFor(text) {
     try {
-        // Retrieve the bearer token from twitter.
-        const response = await user.getBearerToken();
+        let response = await user.getBearerToken();
         console.log(`Got the following Bearer token from Twitter: ${response.access_token}`);
-        
-        // Construct our API client with the bearer token.
+
         const app = new Twitter({
             bearer_token: response.access_token,
         });
-    } catch(e) {
+
+        response = await app.get(`/search/tweets`, {
+            q: text, // The search term
+            lang: "en",        // Let's only get English tweets
+            count: 100,        // Limit the results to 100 tweets
+        });
+
+        let allTweets = "";
+        for (tweet of response.statuses) {
+            allTweets += tweet.text + "\n";
+        }
+
+        const sentimentScore = await getSentimentScore(allTweets);
+        console.log(`The sentiment about text  is: ${sentimentScore}`);
+
+
+    } catch (e) {
         console.log("There was an error calling the Twitter API.");
         console.dir(e);
     }
-})();
+};
+
+checkFor("apple");
+
+
