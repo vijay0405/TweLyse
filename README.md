5a95cace2e79703627b731a3122f97d4433ac0aa
vijay0405 <uvijaysimha@gmail.com>
2020-05-06 22:27:02 +0530

    refactored the code

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # TweLyse
-Twitter sentiment analysis tool.
-score of the sentiment ranges between -1.0 (negative) and 1.0 (positive) and corresponds to the overall emotional leaning of the text
+Twitter sentiment analysis tool
+
+score of the sentiment ranges between -1.0 (negative) and 1.0 (positive) and corresponds to the overall emotional leaning of the text.
+
+
+To run application :
+
+Replace Twitter api key and secret with yours. and enable google nlp api in your google cloud console and download private key file and change the path in package.json.
+
+And use below command in terminal
+
+npm run start
--- index.js
+++ index.js
@@ -1,61 +1,5 @@
-const Twitter = require('twitter-lite');
-const keys = require('./keys.js');
-const language = require('@google-cloud/language');
-const languageClient = new language.LanguageServiceClient();
+const getScore = require('./sentimentScore.js');
 var prompt = require('prompt');
-
-
-
-const user = new Twitter({
-    consumer_key: keys.APIkey,
-    consumer_secret: keys.APIsecretkey,
-});
-
-async function getSentimentScore(text) {
-    const document = {
-        content: text,
-        type: 'PLAIN_TEXT',
-    };
-
-    const [result] = await languageClient.analyzeSentiment({ document: document });
-    const sentiment = result.documentSentiment;
-    console.log(sentiment);
-    return sentiment.score;
-}
-
-
-async function checkFor(text) {
-    try {
-        let response = await user.getBearerToken();
-        // console.log(`Got the following Bearer token from Twitter: ${response.access_token}`);
-
-        const app = new Twitter({
-            bearer_token: response.access_token,
-        });
-
-        response = await app.get(`/search/tweets`, {
-            q: text, // The search term
-            lang: "en",     
-            count: 100,        // Limit the results to 100 tweets
-        });
-
-        let allTweets = "";
-        for (tweet of response.statuses) {
-            allTweets += tweet.text + "\n";
-        }
-
-        const sentimentScore = await getSentimentScore(allTweets);
-        console.log(`The sentiment about ${text} is: ${sentimentScore}`);
-
-
-    } catch (e) {
-        console.log("There was an error calling the Twitter API.");
-        console.dir(e);
-    }
-};
-
-// checkFor("apple");
-
 
 prompt.start();
 
@@ -63,7 +7,7 @@ prompt.get([{
     name: 'searchTerm',
     required: true
   }], function (err, result) {
-    checkFor(result.searchTerm);
+    getScore(result.searchTerm);
   });
 
 
